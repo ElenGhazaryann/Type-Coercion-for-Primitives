e08a1c7767bda5cb89ee5f964a044d26654d69ef
Elen <elen.ghazaryan6@gmail.com>
2021-12-09 01:52:52 +0400

    feat: check math operations

--- t1_testForce.js
+++ t1_testForce.js
@@ -6,3 +6,7 @@ isNaN(" "); // =false
 // at first converts " " to number,  " " =0 , and 0 isn't NaN
 
 +""; // converts  "" to number, " " is 0
+
+0.1 + 0.2 == 0.3; // false // .1 + .2 == .3 is 0.300...004, due to floating point
+9007199254740992 + 1 == 9007199254740992; // false // 2**53 is not safe integer
+9007199254740992 + 2 == 9007199254740992; // false,
