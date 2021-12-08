ac4afde01f8a8bdb9761771923ebf389b9894253
Elen <elen.ghazaryan6@gmail.com>
2021-12-09 01:50:43 +0400

    check whether isNan

--- t1_testForce.js
+++ t1_testForce.js
@@ -1,3 +1,6 @@
 Number("0x77") === 0x77; // false, conv to hexadecimal number
 Number("077") === 077;
 Number(" 12"); // =12 // removes spaces from  start and from end, and returns number or NaN
+
+isNaN(" "); // =false
+// at first converts " " to number,  " " =0 , and 0 isn't NaN
