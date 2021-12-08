dd9d9f8785d56da9fcfa0ee2eb3c32e91244de20
Elen <elen.ghazaryan6@gmail.com>
2021-12-09 00:59:19 +0400

    feat: type coercion string to number

--- t2_coercionPrimitives.js
+++ t2_coercionPrimitives.js
@@ -1,2 +1,5 @@
 "" + 1 + 0; // 10
 // "" is string. while string + number, number converts to string, and concats
+
+"" - 1 + 0; //-1
+// - sign is not determined for string. That's why string converts to number, Number("") = 0
